940d29d4fc98c1d6ff2ce31cad0e316e89699c36
Ivan Litvak <vanya.litvak@gmail.com>
2026-09-02 22:41:47 +0300

    Pin variance and skip rounding diagnostic v6

--- kaggle_artgor_pallas_same_suffix/run_same_suffix.py
+++ kaggle_artgor_pallas_same_suffix/run_same_suffix.py
@@ -8,7 +8,7 @@ import subprocess
 import sys
 
 
-COMMIT_SHA = "6265c83956ef038e1010e88a695ee257696606d2"
+COMMIT_SHA = "53f42155406bda72aee72ea8f10cf5927da68f2d"
 REPOSITORY = "https://github.com/TryDotAtwo/TPUBeamSearch.git"
 CHECKOUT = Path("/tmp/TPUBeamSearch-artgor-pallas-same-suffix")
 OUTPUT = Path("/kaggle/working/artgor_pallas_same_suffix")
