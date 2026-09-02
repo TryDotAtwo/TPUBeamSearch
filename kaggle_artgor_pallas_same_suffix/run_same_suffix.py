2ad37e187785d704235660326064d51003fbac7c
Ivan Litvak <vanya.litvak@gmail.com>
2026-09-02 22:05:56 +0300

    Pin matched-input diagnostic v4

--- kaggle_artgor_pallas_same_suffix/run_same_suffix.py
+++ kaggle_artgor_pallas_same_suffix/run_same_suffix.py
@@ -8,7 +8,7 @@ import subprocess
 import sys
 
 
-COMMIT_SHA = "09fd660f22d075213e93f062a7ab4f19630b3c4c"
+COMMIT_SHA = "f1a0a1a731ab992845fe4a81f823e9922ae935e0"
 REPOSITORY = "https://github.com/TryDotAtwo/TPUBeamSearch.git"
 CHECKOUT = Path("/tmp/TPUBeamSearch-artgor-pallas-same-suffix")
 OUTPUT = Path("/kaggle/working/artgor_pallas_same_suffix")
