f91d4eb92e0c3b6418e53d5747fc3940ea8896fe
Ivan Litvak <vanya.litvak@gmail.com>
2026-09-15 21:36:54 +0300

    Pin response isolation V3 diagnostic source

--- kaggle_beam_response_isolation/run.py
+++ kaggle_beam_response_isolation/run.py
@@ -5,7 +5,7 @@ from pathlib import Path
 import subprocess
 import sys
 
-COMMIT_SHA='5cba03c052f07ae35d75aa57b74304d3fb5e4197'
+COMMIT_SHA='6b8b4e28537e8d6681492d1fefd28132652a3cb6'
 CHECKOUT=Path('/tmp/TPUBeamSearch-response-isolation')
 OUTPUT=Path('/kaggle/working/beam_response_isolation')
 
